b798ae904b22e673fb3c63cf3b10d51ee668f8b9
Isabella <127263818+IsabellaSluyter@users.noreply.github.com>
2024-03-29 13:34:34 -0400

    Update solarsystem.py

--- solarsystem.py
+++ solarsystem.py
@@ -1,1 +1,15 @@
+#solarsystem.py
 
+import turtle
+
+class SolarSystemBody(turtle.Turtle):
+  ...
+
+class Sun(SolarSystemBody):
+  ...
+
+class Planet(SolarSystemBody):
+  ...
+
+class SolarSystem:
+  ...
