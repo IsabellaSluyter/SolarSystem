6cb065c21acbdf52893782a18c22fb24d014163d
Isabella <127263818+IsabellaSluyter@users.noreply.github.com>
2024-03-29 13:52:52 -0400

    Update solarsystem.py

--- solarsystem.py
+++ solarsystem.py
@@ -1,12 +1,33 @@
 #solarsystem.py
 
+import math
 import turtle
 
 class SolarSystemBody(turtle.Turtle):
-  ...
+  minDisplaySize = 20
+  displayLogBase = 1.1
+  
+  def __init__(self, solarSystem, mass, position=(0,0), velocity=(0,0)):
+    super().__init__()
+    self.mass = mass
+    self.setposition(position)
+    self.velocity = velocity
+    self.displaySize = max(math.log(self.mass, self.displayLogBase), self.minDisplaySize)
+
+    #removes lines
+    self.penup()
+    #hides object that draws
+    self.hideturtle()
+
+    solarSystem.addBody(self)
+
+  def draw(self):
+    self.dot(self.displaySize)
 
 class Sun(SolarSystemBody):
-  ...
+  def __init__(self, solarSystem, mass, position=(0, 0), velocity=(0, 0)):
+        super().__init__(solarSystem, mass, position, velocity)
+        self.color("yellow")
 
 class Planet(SolarSystemBody):
   ...
