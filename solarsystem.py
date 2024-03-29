a2c411dff86333c42a995e7d7317499f6cddfbee
Isabella <127263818+IsabellaSluyter@users.noreply.github.com>
2024-03-29 13:41:12 -0400

    Update solarsystem.py

--- solarsystem.py
+++ solarsystem.py
@@ -12,4 +12,18 @@ class Planet(SolarSystemBody):
   ...
 
 class SolarSystem:
-  ...
+  def __init__(self, width, height):
+    self.solarSystem = turtle.Screen()
+    self.solarSystem.tracer(0)
+    self.solarSystem.setup(width, height)
+    self.solarSystem.bgcolor("black")
+
+    #stores list of bodies present
+    self.bodies = [0]
+    
+  def addBody(self, body):
+    self.bodies.append(body)
+
+  def removeBody(self, body):
+    self.bodies.remove(body)
+
