22d54dfb32e5364d430071a78f93c89d2703eef2
Isabella <127263818+IsabellaSluyter@users.noreply.github.com>
2024-03-31 22:28:36 -0400

    Update simpleSolarSystem.py

--- simpleSolarSystem.py
+++ simpleSolarSystem.py
@@ -2,9 +2,10 @@ from solarsystem import SolarSystem, Sun
 
 solar_system = SolarSystem(width=1400, height=900)
 
-sun = Sun(solar_system, mass=10_000)
+sun = Sun(solar_system, mass=10_000, velocity=(2, 1))
 
-sun.draw()
+while True:
+  solar_system.update_all()
 
 # temp lines
 import turtle
