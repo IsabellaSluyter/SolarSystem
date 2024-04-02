31e4ffbd7a3954220ed532a59695ee1a52fc7f1f
Isabella <127263818+IsabellaSluyter@users.noreply.github.com>
2024-04-01 22:48:29 -0400

    Update simpleSolarSystem.py w/ planets

--- simpleSolarSystem.py
+++ simpleSolarSystem.py
@@ -1,8 +1,9 @@
-from solarsystem import SolarSystem, Sun
+from solarsystem import SolarSystem, Sun, Planet
 
 solar_system = SolarSystem(width=1400, height=900)
 
-sun = Sun(solar_system, mass=10_000, velocity=(2, 1))
+sun = Sun(solar_system, mass=10_000)
+planet = Planet(solar_system, mass=1, position=(-350, 0), velocity=(0, 5))
 
 while True:
   solar_system.update_all()
