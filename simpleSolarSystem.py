3e77adb9048b10659ad50dae3f6329a6300a1f57
Isabella <127263818+IsabellaSluyter@users.noreply.github.com>
2024-04-02 12:13:35 -0400

    Update simpleSolarSystem.py w/ gravity

--- simpleSolarSystem.py
+++ simpleSolarSystem.py
@@ -6,4 +6,5 @@ sun = Sun(solar_system, mass=10_000)
 planet = Planet(solar_system, mass=1, position=(-350, 0), velocity=(0, 5))
 
 while True:
+  solar_system.accelerate_due_to_gravity(sun, planet)
   solar_system.update_all()
